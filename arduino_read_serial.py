0209a348f376a3884abcec74d16dbad13b152878
xr72 <vrbaj@vscht.cz>
2023-05-31 19:09:22 +0200

    new line fixed

--- arduino_read_serial.py
+++ arduino_read_serial.py
@@ -17,7 +17,8 @@ def read_arduino(com_port, baudrate, path_to_file):
                 values = data.split(";")
                 now = datetime.now()
                 values.append(now.strftime("%Y-%m-%d %H:%M:%S"))
-                with open(path_to_file, "w") as f:
+                print(values)
+                with open(path_to_file, "a", newline="") as f:
                     writer = csv.writer(f, delimiter=";")
                     writer.writerow(values)
         sleep(1)
@@ -27,7 +28,7 @@ if __name__ == '__main__':
     path_to_save = Path("temperature.csv")
     if not path_to_save.exists():
         print("file with values does not exist")
-        with open(path_to_save, "w") as f:
+        with open(path_to_save, "w", newline="") as f:
             writer = csv.writer(f, delimiter=";")
             writer.writerow(["temperature", "humidity", "pressure", "datetime"])
     read_arduino('COM6', 9600, path_to_save)
